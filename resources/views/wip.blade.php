c0c3addc3b1df3ac43d73708b96cc68f1ed0b7f8
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-05-24 09:41:15 +0200

    erste anpassung

--- resources/views/wip.blade.php
+++ resources/views/wip.blade.php
@@ -4,17 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>workInProgress</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body style="background-image: url('{{ asset('images/hg.jpg') }}');">
-    
-   <div class="svg container col-6 mt-5">
-      <img src="{{ asset('images/wip.svg') }}" alt="SVG-Bild" style="width:1000px; justify-content: center; border-radius: 50px;">
-     
-     <button type="button" class="btn btn-secondary" style="width:1000px; justify-content: center;border-radius: 50px;">
-        <a href="{{ url('/login') }}" class="btn btn-secondary">Link</a>
-     </button>
+<body style="background-image: url('{{ asset('images/hg.jpg') }}');background-size: cover;background-repeat: no-repeat;background-position: center center; min-height: auto;">
+    <br>
+   <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12">
+         <div class="my-div">
+            <img src="{{ asset('images/wip.svg') }}" alt="SVG-Bild" style="border-radius: 50px; height: auto; width: 1440px;">
+         </div>
+
+         <div>
+            <button type="button" class="btn btn-secondary" style="border-radius: 50px;height: auto; width: 1440px;">
+               <a href="{{ url('/login') }}" class="btn btn-secondary">Link</a>
+            </button>
+         </div>
+
+        </div>
+      </div>
    </div>
+   
 </body>
 </html>
