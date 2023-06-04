2f7a776db07264deb38a40c94b31fa9c53be076c
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-06-04 14:43:43 +0200

    a

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,20 +33,7 @@
         </div>
     </div>
 
-    <div class="bg-white shadow p-5 rounded-sm">
-        <div class="border-b-2 border-blue-700 relative flex items-center justify-center">
-            <h3 class="absolute bg-white px-4 text-xs text-gray-500 font-medium">Neueste Beiträge</h3>
-        </div>
-        <div class="mt-5 text-gray-700 text-sm">
-            <ul>
-                <li><a href="#">Überschrift Beitrag 1</a></li>
-                <li><a href="#">Überschrift Beitrag 2</a></li>
-                <li><a href="#">Überschrift Beitrag 3</a></li>
-                <li><a href="#">Überschrift Beitrag 4</a></li>
-                <li><a href="#">Überschrift Beitrag 5</a></li>
-            </ul>
-        </div>
-    </div>
+    <x-post.newest/>
 
     <div class="bg-white shadow p-5 rounded-sm">
         <div class="border-b-2 border-blue-700 relative flex items-center justify-center">
