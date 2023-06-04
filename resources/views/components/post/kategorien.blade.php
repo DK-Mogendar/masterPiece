74fc88ad63f1a680d529871304074ff2ad26b8f9
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-06-04 15:03:47 +0200

    components

---
+++ resources/views/components/post/kategorien.blade.php
@@ -0,0 +1,14 @@
+<div class="bg-white shadow p-5 rounded-sm">
+    <div class="border-b-2 border-blue-700 relative flex items-center justify-center">
+        <h3 class="absolute bg-white px-4 text-xs text-gray-500 font-medium">Kategorien</h3>
+    </div>
+    <div class="mt-5 text-gray-700 text-sm">
+        <ul>
+            <li><a href="#">Kategorie Nr. 1</a></li>
+            <li><a href="#">Kategorie Nr. 2</a></li>
+            <li><a href="#">Kategorie Nr. 3</a></li>
+            <li><a href="#">Kategorie Nr. 4</a></li>
+            <li><a href="#">Kategorie Nr. 5</a></li>
+        </ul>
+    </div>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,35 +35,9 @@
 
     <x-post.newest/>
 
-    <div class="bg-white shadow p-5 rounded-sm">
-        <div class="border-b-2 border-blue-700 relative flex items-center justify-center">
-            <h3 class="absolute bg-white px-4 text-xs text-gray-500 font-medium">Kategorien</h3>
-        </div>
-        <div class="mt-5 text-gray-700 text-sm">
-            <ul>
-                <li><a href="#">Kategorie Nr. 1</a></li>
-                <li><a href="#">Kategorie Nr. 2</a></li>
-                <li><a href="#">Kategorie Nr. 3</a></li>
-                <li><a href="#">Kategorie Nr. 4</a></li>
-                <li><a href="#">Kategorie Nr. 5</a></li>
-            </ul>
-        </div>
-    </div>
+    <x-post.kategorien/>
 
-    <div class="bg-white shadow p-5 rounded-sm">
-        <div class="border-b-2 border-blue-700 relative flex items-center justify-center">
-            <h3 class="absolute bg-white px-4 text-xs text-gray-500 font-medium">Trending Stories</h3>
-        </div>
-        <div class="mt-5 text-gray-700 text-sm">
-            <ul>
-                <li><a href="#">Trending Story No. 1</a></li>
-                <li><a href="#">Trending Story No. 2</a></li>
-                <li><a href="#">Trending Story No. 3</a></li>
-                <li><a href="#">Trending Story No. 4</a></li>
-                <li><a href="#">Trending Story No. 5</a></li>
-            </ul>
-        </div>
-    </div>
+    <x-post.trending-stories/>
 
 </div>
 </x-layouts.main>
