dff4789dd1e4518e9940a8a276b815bf7ab7b9ae
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-06-04 17:14:54 +0200

    trending and categorie

--- resources/views/components/post/trending.blade.php
+++ resources/views/components/post/trending.blade.php
@@ -1,14 +1,12 @@
 <div class="bg-white shadow p-5 rounded-sm">
     <div class="border-b-2 border-blue-700 relative flex items-center justify-center">
-        <h3 class="absolute bg-white px-4 text-xs text-gray-500 font-medium">Trending Stories</h3>
+        <h3 class="absolute bg-white px-4 text-xs text-gray-500 font-medium">Interssante Beiträge</h3>
     </div>
     <div class="mt-5 text-gray-700 text-sm">
         <ul>
-            <li><a href="#">Trending Story No. 1</a></li>
-            <li><a href="#">Trending Story No. 2</a></li>
-            <li><a href="#">Trending Story No. 3</a></li>
-            <li><a href="#">Trending Story No. 4</a></li>
-            <li><a href="#">Trending Story No. 5</a></li>
+            @foreach ($posts as $post)
+            <li><a href="/post/{{$post['id']}}">{{$post['title']}}</a></li>
+            @endforeach
         </ul>
     </div>
 </div>
