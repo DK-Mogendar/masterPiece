bf807cc852d4a2ecf5b04848ea384e98ac06cc82
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-06-06 12:13:41 +0200

    mittags commit sicherheit

--- app/Http/Controllers/StartpageContoller.php
+++ app/Http/Controllers/StartpageContoller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class StartpageContoller extends Controller
@@ -9,7 +10,11 @@ class StartpageContoller extends Controller
     public function index()
     {
        
-        $posts =[];
+        $posts = Post::all()->sortByDesc(callback:'created_at');
+        //lezten 5 beiträge
+        //sortieren nach neueste oben
+        //als gelöscht markierte sollen ausgeblendent werden
+
         return view('index', compact('posts'));
     }
 }
