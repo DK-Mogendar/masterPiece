602ef4969704fc192a9666c5e80862b95c5cc8fa
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-06-27 10:39:08 +0200

    Host lösung

--- vite.config.js
+++ vite.config.js
@@ -7,54 +7,12 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
-    /*
-    server: {
-        https: true
-    },
-    base: '/'   */
+
 });
 
-
-/*
-export default defineConfig(({ command, mode, ssrBuild }) => {
-    if (command === 'build') {
-      return {
-        // production-specific config
-        plugins: [
-            laravel({
-                input: [
-                    'resources/css/app.css',
-                    'resources/sass/app.scss',
-                    'resources/js/app.js',
-                ],
-                refresh: true,
-            }),
-        ],
-        server: {
-            https: true
-        },
-        base: '/' 
-      }
-    } else {
-      return {
-        // dev-specific config
-        plugins: [
-            laravel({
-                input: [
-                    'resources/css/app.css',
-                    'resources/sass/app.scss',
-                    'resources/js/app.js',
-                ],
-                refresh: true,
-            }),
-        ]
-      }
-    }
-})*/
 
