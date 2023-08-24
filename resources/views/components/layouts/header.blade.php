83c059e0e0997a1b35e36cf828e1b28ddf46d173
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-08-24 10:51:29 +0200

    welcom umfunktioniert zu Quellennachweis

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,11 +1,12 @@
 <header class="bg-white-25 border-t-4 border-blue-700 shadow mb-5">   
-    <nav class="container max-w-7xl mx-auto px-5 lg:px-40 py-5 flex justify-between text-2xl">
+    <nav class="container max-w-7xl mx-auto px-5 lg:px-40 py-5 flex justify-between text-xl">
         <ul class="flex space-x-5">
             <a href="{{ url('/') }}" class="font-bold mr-5 text-blue-700">Mogendar-Art-Blog</a>
             <li><a href="{{ url('start') }}" class="btn btn-secondary font-bold mr-5 text-blue-700" style="height: auto; width: 100%;border-radius: 50px;">Home/Start</a></li>
             <!--<li><a href="{{ url('welcome') }}" class="btn btn-secondary" style="height: auto; width: 100%;border-radius: 50px;">Archiv Laravel</a></li>-->
             <li><a href="{{ url('test') }}" class="btn btn-secondary font-bold mr-5 text-blue-700" style="height: auto; width: 100%;border-radius: 50px;">News</a></li>
             <li><a href="{{ url('aboutMy') }}" class="btn btn-secondary font-bold mr-5 text-blue-700" style="height: auto; width: 100%;border-radius: 50px;">About me</a></li>
+            <li><a href="{{ url('welcome') }}" class="btn btn-secondary font-bold mr-5 text-blue-700" style="height: auto; width: 100%;border-radius: 50px;">Quellen</a></li>
         </ul>
 
         <ul>
